aac632aeac2fd8abc7007ebda36b9dcadffa46ef
Masahiro Hirano <masahiro.dll@gmail.com>
2018-03-01 17:55:41 +0900

    Revert "delete"
    This reverts commit af96809a2b80196304b97636f3b89c59661a16c7.

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,8 @@ int main(int argc, char *argv[])
     if (engine.rootObjects().isEmpty())
         return -1;
 
+    // QObject* root = engine.rootObjects().first();
+
     return app.exec();
     
 }
